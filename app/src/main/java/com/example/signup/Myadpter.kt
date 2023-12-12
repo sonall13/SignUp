dc799ce56cd1d802a8486d3627385b05db1aba1b
sonal <sonaambaliya005@gmail.com>
2023-12-12 23:30:41 +0530

    34

--- app/src/main/java/com/example/signup/Myadpter.kt
+++ app/src/main/java/com/example/signup/Myadpter.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 
@@ -33,6 +35,12 @@ class Myadpter(
         var editt : ImageView
         var delete : ImageView
 
+        var editname : EditText
+        var editcontact : EditText
+        var editemail : EditText
+        var update : Button
+        var dialogcancel : Button
+
         var forcontact = LayoutInflater.from(homePage).inflate(R.layout.forcontact,p2,false)
 
         textview=forcontact.findViewById(R.id.textview)
@@ -42,12 +50,24 @@ class Myadpter(
 
         var dialog = Dialog(homePage)
 
+//        editname=dialog.findViewById(R.id.editname)
+//        editcontact=dialog.findViewById(R.id.editcontact)
+//        editemail=dialog.findViewById(R.id.editemail)
+//        update=dialog.findViewById(R.id.update)
+//        dialogcancel=dialog.findViewById(R.id.dialogcancel)
+
+
+
         editt.setOnClickListener {
 
             dialog.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT)
-            dialog.setCancelable(false)
+//            dialog.setCancelable(false)
             dialog.setContentView(R.layout.dialogbox)
+
+
+
         }
+
 
         textview.setText(numarray[p0])
         textview2.setText(conarray[p0])
